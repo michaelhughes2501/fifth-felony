190b234c19be91549d5a1ae9abd114d9358fc69c
mike hughes <michaelhughes2501@gmail.com>
2026-08-07 20:55:21 -0600

    chore: add Next.js ESLint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextVitals,
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "coverage/**",
+      "dist/**",
+      "agent-reports/**",
+    ],
+  },
+]);
